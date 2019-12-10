ecdc0a0a1ea43b9fdc62e2608881ed51e10f21b4
Daniel Chow <DChow@spartaglobal.com>
2019-12-10 10:49:03 +0000

    all tests passing with pytest

--- test_unittest_simplecalc.py
+++ test_unittest_simplecalc.py
@@ -7,12 +7,17 @@ class Calctest(unittest.TestCase):
 
     def test_add(self):
         self.assertEqual(self.calc.add(2, 4), 6)
+        self.assertEqual(self.calc.add(4, 4), 8)
 
     def test_subtract(self):
         self.assertEqual(self.calc.subtract(4, 2), 2)
+        self.assertEqual(self.calc.subtract(5, 4), 1)
 
     def test_multiply(self):
         self.assertEqual(self.calc.multiply(2, 3), 6)
+        self.assertEqual(self.calc.multiply(2, 2), 4)
 
     def test_divide(self):
         self.assertEqual(self.calc.divide(8, 4), 2)
+        self.assertEqual(self.calc.divide(9, 3), 3)
+
